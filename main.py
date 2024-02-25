8475f3e6ebbf267bb991f048360ef9dd41b61750
Cesar Roman <cesardroman18@gmail.com>
2024-02-24 18:22:11 -0600

    style:renamed todos to all_todos for clarity

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ class Todo(BaseModel):
 
 
 sample_todo = Todo(id=1, name="Sample todo")
-todos: List[Todo] = [sample_todo]
+global_todos: List[Todo] = [sample_todo]
 
 
 # Get Todos
 @app.get("/todos")
 async def get_todos():
-    return {"todos": todos}
+    return {"todos": global_todos}
 
 
 # Get Todo by id
 @app.get("/todos/{todo_id}")
 async def get_todo(todo_id: int):
-    for todo in todos:
+    for todo in global_todos:
         if todo.id == todo_id:
             return {"todo": todo}
 
@@ -34,7 +34,7 @@ async def get_todo(todo_id: int):
 # Create Todo
 @app.post("/todos")
 async def create_todo(new_todo: Todo):
-    todos.append(new_todo)
+    global_todos.append(new_todo)
 
     return {f"'message': 'Todo {new_todo.name} successfully created!'"}
 
@@ -42,7 +42,7 @@ async def create_todo(new_todo: Todo):
 # Update Todo
 @app.put("/todos")
 async def update_todo(updated_todo: Todo):
-    for todo in todos:
+    for todo in global_todos:
         if todo.id == updated_todo.id:
             todo.id = updated_todo.id
             todo.name = updated_todo.name
@@ -55,8 +55,8 @@ async def update_todo(updated_todo: Todo):
 # Delete Todo by id
 @app.delete("/todos/{todo_id}")
 async def delete_todo(todo_id: int):
-    global todos
-    updated_todos = filter(lambda todo: todo.id != todo_id, todos)
-    todos[:] = list(updated_todos)
+    global global_todos
+    updated_todos = filter(lambda todo: todo.id != todo_id, global_todos)
+    global_todos[:] = list(updated_todos)
 
     return {"message": "Todo successfully deleted"}
